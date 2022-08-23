18e35be81b1d7c845624d92684d9ac97a7371eff
Tejas125 <tejassanjayjadhav@gmail.com>
2022-08-23 12:36:41 +0530

    updated Shipper Name

--- src/main/java/com/shivtejworld/storemanagementapp/controller/ShipperController.java
+++ src/main/java/com/shivtejworld/storemanagementapp/controller/ShipperController.java
@@ -1,67 +1,49 @@
 package com.shivtejworld.storemanagementapp.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.shivtejworld.storemanagementapp.model.Shipper;
-import com.shivtejworld.storemanagementapp.repository.ShipperRepository;
+import com.shivtejworld.storemanagementapp.service.ShipperService;
 
 @RestController
 @RequestMapping("/shippers")
 public class ShipperController {
 
 	@Autowired
-	private ShipperRepository shipperRepository;
-
+	private ShipperService shipperService;
+	
 	@GetMapping()
-	public List<Shipper> getShippers() {
-		List<Shipper> shippersList = new ArrayList<Shipper>();
-
-		Iterable<Shipper> iterator = shipperRepository.findAll();
-		iterator.forEach(shipper -> shippersList.add(shipper));
-		
-		return shippersList;
-	}
-
-	@PutMapping
-	public String putShippers() {
-		return "PUT Shippers";
+	public List<Shipper> getAllShippers() {
+		return shipperService.getAllShippers();
 	}
 
 	@DeleteMapping
-	public String deleteShippers(@RequestParam Long shipperID) {
-		shipperRepository.deleteById(shipperID);
-		return "Deleted Shipper with ShipperID "+shipperID+" .";
+	public String deleteShipperById(@RequestParam Long shipperID) {
+		return shipperService.deleteShipperById(shipperID);
 	}
 
 	@PostMapping
-	public Shipper postShippers(@RequestBody Shipper shippers) {
-		return shipperRepository.save(shippers);
+	public Shipper saveShipper(@RequestBody Shipper shipper) {
+		return shipperService.saveShipper(shipper);
 	}
 
-	@PatchMapping
+	@PatchMapping("/updateShipperPhone")
 	public String updateShipperPhone(@RequestParam Long shipperID, @RequestParam Integer phone) {
-		Optional<Shipper> myShipper = shipperRepository.findById(shipperID);
-		if(myShipper.isPresent()) {
-			myShipper.get().setPhone(phone);
-			shipperRepository.save(myShipper.get());
-			return "Phone Updated for Shipper with ShipperID " + shipperID;
-		}
-		else {
-			return "Shipper with ShipperID " + shipperID + " Not Available";
-		}
+		return shipperService.updateShipperPhone(shipperID, phone);
 	}
+	
+	@PatchMapping("/updateShipperName")
+	public String updateShipperName(@RequestParam Long shipperID, @RequestParam String shipperName) {
+		return shipperService.updateShipperName(shipperID, shipperName);
+	}
+
 	
 }
--- src/main/java/com/shivtejworld/storemanagementapp/service/ShipperService.java
+++ src/main/java/com/shivtejworld/storemanagementapp/service/ShipperService.java
@@ -1,5 +1,59 @@
 package com.shivtejworld.storemanagementapp.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.shivtejworld.storemanagementapp.model.Shipper;
+import com.shivtejworld.storemanagementapp.repository.ShipperRepository;
+
+@Service
 public class ShipperService {
 
+	@Autowired
+	private ShipperRepository shipperRepository;
+
+	public List<Shipper> getAllShippers(){
+		List<Shipper> shippersList = new ArrayList<Shipper>();
+
+		Iterable<Shipper> iterator = shipperRepository.findAll();
+		iterator.forEach(shipper -> shippersList.add(shipper));
+	
+		return shippersList;
+	}
+	
+	public String deleteShipperById(Long shipperID) {
+		shipperRepository.deleteById(shipperID);
+		return "Deleted Shipper with ShipperID "+shipperID+" .";
+	}
+	
+	public Shipper saveShipper(Shipper shipper) {
+		return shipperRepository.save(shipper);
+	}
+	
+	public String updateShipperPhone(Long shipperID, Integer phone){
+		Optional<Shipper> myShipper = shipperRepository.findById(shipperID);
+		if(myShipper.isPresent()) {
+			myShipper.get().setPhone(phone);
+			shipperRepository.save(myShipper.get());
+			return "Phone Updated for Shipper with ShipperID " + shipperID;
+		}
+		else {
+			return "Shipper with ShipperID " + shipperID + " Not Available";
+		}
+	
+	}
+
+	public String updateShipperName(Long shipperID, String shipperName) {
+		Optional<Shipper> myShipper = shipperRepository.findById(shipperID);
+		if(myShipper.isPresent()) {
+			myShipper.get().setShipperName(shipperName);
+			shipperRepository.save(myShipper.get());
+			return "ShipperName Updated for Shipper with ShipperID " + shipperID;
+		}
+		else {
+			return "Shipper with ShipperID " + shipperID + " Not Available";
+		}
+	}
 }
