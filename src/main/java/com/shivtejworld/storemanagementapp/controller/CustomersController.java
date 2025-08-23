0831bc57e7bd1962be10b8b8e8ff6a4d48e813fd
Tejas <tejassanjayjadhav@gmail.com>
2025-08-23 19:54:44 +0530

    Review functionality
    Review new functionality
    
    Co-Authored-By: ShivaniTejasJadhav <217813262+ShivaniTejasJadhav@users.noreply.github.com>

--- src/main/java/com/shivtejworld/storemanagementapp/controller/CustomersController.java
+++ src/main/java/com/shivtejworld/storemanagementapp/controller/CustomersController.java
@@ -1,7 +1,5 @@
-package 
-com.shivtejworld.storemanagementapp.controller;
 
- 
+package com.shivtejworld.storemanagementapp.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,66 +12,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
- 
-
 import com.shivtejworld.storemanagementapp.model.Customers;
 import com.shivtejworld.storemanagementapp.service.CustomersService;
-
- 
 
 @RestController
 @RequestMapping("/customers")
 public class CustomersController {
 
- 
+	@Autowired(required = true)
+	private CustomersService customersService;
 
-@Autowired(required=true)
-private CustomersService CustomersService;
+	@CrossOrigin
+	@GetMapping
+	public List<Customers> getAllCustomers() {
+		return customersService.getAllCustomers();
+	}
 
- 
+	@CrossOrigin
+	@DeleteMapping
+	public String deleteCustomerById(@RequestParam Long customerID) {
+		return customersService.deleteCustomersById(customerID);
+	}
 
-@CrossOrigin
-@GetMapping
-public List<Customers> getAllCustomers() {
-return CustomersService.getAllCustomers();
-}
+	@CrossOrigin
+	@PostMapping
+	public Customers saveCustomer(@RequestBody Customers cust) {
+		System.out.println(cust);
+		return customersService.saveCustomer(cust);
+	}
 
- 
-
-@CrossOrigin
-@DeleteMapping
-public String deleteCustomerById(@RequestParam Long CustomerID) { 
-return CustomersService.deleteCustomersById(CustomerID);
-}
-
- 
-
-@CrossOrigin
- 
-@PostMapping public Customers saveCustomer(@RequestBody Customers cust) {
-System.out.println(cust); 
-return CustomersService.saveCustomer(cust);
-}
-
-/* 
- * 
- * 
- * @CrossOrigin
- * 
- * @PatchMapping public String updateCustomer(@RequestParam Long CustomersID,
- * 
- * @RequestParam String CustomerName,
- * 
- * @RequestParam String ContactName,
- * 
- * @RequestParam String Address,
- * 
- * @RequestParam String City,
- * 
- * @RequestParam long PostalCode,
- * 
- * @RequestParam String Country) { return
- * CustomersService.updateCustomer(CustomerID,CustomerName,ContactName,Address,
- * City,PostalCode,Country); }
- */
+	/*
+	@CrossOrigin
+	@PatchMapping
+	public String updateCustomer(
+			@RequestParam Long customerID,
+			@RequestParam String customerName,
+			@RequestParam String contactName,
+			@RequestParam String address,
+			@RequestParam String city,
+			@RequestParam long postalCode,
+			@RequestParam String country) {
+		return customersService.updateCustomer(customerID, customerName, contactName, address, city, postalCode, country);
+	}
+	*/
 }
--- src/main/java/com/shivtejworld/storemanagementapp/controller/OrderDetailsController.java
+++ src/main/java/com/shivtejworld/storemanagementapp/controller/OrderDetailsController.java
@@ -2,7 +2,6 @@ package com.shivtejworld.storemanagementapp.controller;
 
 import java.util.Date;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.shivtejworld.storemanagementapp.model.OrderDetails;
 import com.shivtejworld.storemanagementapp.service.OrderDetailsService;
 
@@ -21,33 +19,37 @@ import com.shivtejworld.storemanagementapp.service.OrderDetailsService;
 @RequestMapping("/orderdetails")
 public class OrderDetailsController {
 
-	@Autowired(required=true)
-	private OrderDetailsService OrderDetailsService;
-	
-	
+	@Autowired(required = true)
+	private OrderDetailsService orderDetailsService;
+
 	@CrossOrigin
 	@GetMapping
 	public List<OrderDetails> getAllOrderDetails() {
-		return OrderDetailsService.getAllOrderDetails();
+		return orderDetailsService.getAllOrderDetails();
 	}
 
 	@CrossOrigin
 	@DeleteMapping
 	public String deleteOrderById(@RequestParam Long orderDetailID) {
-		return OrderDetailsService.deleteOrderDetailById(orderDetailID);
+		return orderDetailsService.deleteOrderDetailById(orderDetailID);
 	}
 
 	@CrossOrigin
 	@PostMapping
 	public OrderDetails saveOrderDetail(@RequestBody OrderDetails orderDetail) {
 		System.out.println(orderDetail);
-		return OrderDetailsService.saveOrderDetail(orderDetail);
+		return orderDetailsService.saveOrderDetail(orderDetail);
 	}
-	
+
 	@CrossOrigin
 	@PatchMapping
-	public String updateOrderDetails(@RequestParam Long orderID, @RequestParam Long customerID, @RequestParam Long employeeID, @RequestParam Date orderDate, @RequestParam Long shipperID) {
-		return OrderDetailsService.updateOrderDetail(orderID,customerID,employeeID,orderDate,shipperID);
+	public String updateOrderDetails(
+			@RequestParam Long orderID,
+			@RequestParam Long customerID,
+			@RequestParam Long employeeID,
+			@RequestParam Date orderDate,
+			@RequestParam Long shipperID) {
+		return orderDetailsService.updateOrderDetail(orderID, customerID, employeeID, orderDate, shipperID);
 	}
-	
+
 }
