f7b7d39b561d26b8a380f5af31161de3461eb307
Tejas <tejassanjayjadhav@gmail.com>
2025-08-29 13:25:01 +0530

    Remove unused imports from model and service classes

--- src/main/java/com/shivtejworld/storemanagementapp/model/Shipper.java
+++ src/main/java/com/shivtejworld/storemanagementapp/model/Shipper.java
@@ -5,15 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="shippers")
 public class Shipper {
-	
-	private static final long serialVersionUID = 1L;
-	
+		
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "shipperid")
